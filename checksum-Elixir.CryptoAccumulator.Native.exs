987a5c91e6156255afae640a5c7793ecf977720a
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-03-21 11:27:39 +0100

    Add checksums

---
+++ checksum-Elixir.CryptoAccumulator.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "crypto_accumulator-v0.1.0-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:100fe634c4fcef78c6f36e5915c47760060e2dc094729f73f2669cb4449b8586",
+  "crypto_accumulator-v0.1.0-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:f3dea201f13d029b4f7bfb250cf7f6de7ec01f7d1cc7ab193cb64114a1759e19",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:1b2faf7a399b6779387b478d2a8f92d727fa62fe19136d3c7c15074055c35fc5",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e8605b505cb8cccfaf98f0558b35d121c8922a2a7bdef9395b635984dff750a9",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:8e5f05ac01cc431d9e8a91b5ca99d65d434ed584da888787470565602fb4c133",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:5f091df24477c2b78d7fb5a280fd7a2ce6e603bf5b26aef67c9ce24a2243d29e",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:73857fe0ca888a750ad92c6b73793322e926ad63b2a69172c4ef536c66e2a914",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:e0344297323db10458e7fd2dff6dd8ae2f5aff36f8995e8458c59d5081753042",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f56a8cb267afb70f5e2ffc28aa5a9b9973a471ab19cb2143ebe612f617e9f235",
+  "libcrypto_accumulator-v0.1.0-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:b60886c6d604479b602a364844667489eaaa20228ee5d4bedb3d774eb6b20296",
+}
